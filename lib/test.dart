3f30e797c32b501e9745cc7ea774f0d0e8704553
손진성 <felixson99@gmail.com>
2025-03-20 18:14:04 +0900

    주석 추가

--- lib/test.dart
+++ lib/test.dart
@@ -1,13 +1,29 @@
+/*
+수정해야할 사항
+
+1. 주석추가
+2. 몬스터의 공격력이 지속적으로 감소하는 현상 수정
+3. 방어 논리 수정(방어시 공격받은 수치를 회복해야하는데 공격받지 않아도(첫 번째 턴)체력 회복하는 논리)
+4. 메시지 출력간 2초의 지연시간 추가
+5. 세밀한 게임 방식의 논리 오류 수정
+6. 가독성과 유지보수성을 위한 코드 분리 시도
+
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+이번 수정사항
+1. 주석추가
+
+*/
+
 import 'dart:io';
 import 'dart:math';
 
-class User {
+class User {                           // 사용자 클래스 정의 
   String? username;
   int userHp;
   int userAttack;
   int userDefense;
 
-  User({
+  User({                               // 초기값 설정(생성자) 
     required this.username,
     required this.userHp,
     required this.userAttack,
@@ -15,12 +31,12 @@ class User {
   });
 }
 
-class Monster {
+class Monster {                        // 몬스터 클래스 정의 
   String monsterName;
   int monsterHp;
   int monsterAttack;
 
-  Monster({
+  Monster({                            // 초기값 설정(생성자)
     required this.monsterName,
     required this.monsterHp,
     required this.monsterAttack,
@@ -28,21 +44,20 @@ class Monster {
 }
 
 class Game {
-  List<Monster> monsterList = [
-    // 제네릭 Monster 타입입
+  List<Monster> monsterList = [        // 몬스터 리스트 초기화
     Monster(monsterName: "spiderman", monsterHp: 20, monsterAttack: 5),
     Monster(monsterName: "thanos", monsterHp: 30, monsterAttack: 5),
     Monster(monsterName: "superman", monsterHp: 30, monsterAttack: 10),
   ];
 
-  User player = User(
+  User player = User(                  // 사용자 정보 초기화
     username: "random",
     userHp: 50,
     userAttack: 10,
     userDefense: 5,
   );
 
-  Random random = Random();
+  Random random = Random();            //랜덤 숫자 생성을 위한 random 객체 생성
 
   void mainBattle() {
     print("캐릭터의 이름을 입력하세요:");
@@ -50,25 +65,23 @@ class Game {
     print("게임을 시작합니다!");
     print(
       "${player.username} - 체력: ${player.userHp}, 공격력: ${player.userAttack}, 방어력: ${player.userDefense}",
-    );
+    );                                 // 게임 시작 출력문
 
-    Monster? resultMonster;
-
-    while (player.userHp > 0 && monsterList.isNotEmpty) {
-      int index = random.nextInt(monsterList.length); // 숙지해야할 Random 리스트 코드 방식
+    while (player.userHp > 0 && monsterList.isNotEmpty) {                // 외부 While문 시작(루프시킬 전체 게임구조)
+      int index = random.nextInt(monsterList.length);                    // 숙지해야할 Random 리스트 코드 방식
       Monster resultMonster =
-          monsterList[index]; // nextInt(n) 메서드는 0부터 n-1까지의 정수 중 하나를 랜덤하게 반환합니다.
+          monsterList[index];          // nextInt(n) 메서드는 0부터 n-1까지의 정수 중 하나를 랜덤하게 반환합니다.
 
       print("새로운 몬스터가 나타났습니다!");
       print(
         "${resultMonster.monsterName} - 체력: ${resultMonster.monsterHp}, 공격력: ${resultMonster.monsterAttack}",
       );
 
-      while (player.userHp > 0 && resultMonster.monsterHp > 0) {
+      while (player.userHp > 0 && resultMonster.monsterHp > 0) {          // 내부 While문 시작(루프시킬 전투 구조)
         print("${player.username}의 턴");
-        print("행동을 선택하세요 (1: 공격, 2: 방어) ");
+        print("행동을 선택하세요 (1: 공격, 2: 방어) ");                     
 
-        String? inputData = stdin.readLineSync();
+        String? inputData = stdin.readLineSync();                         // 사용자의 공격패턴 입력받기
         int choice = int.tryParse(inputData ?? "0") ?? 0;
         switch (choice) {
           case 1:
@@ -84,38 +97,37 @@ class Game {
               "${player.username}이(가) 방어 태세를 취하여 ${resultMonster.monsterAttack} 만큼 체력을 얻었습니다.",
             );
             break;
-        }
-        player.userHp -= resultMonster.monsterAttack;
+        }                               // 사용자의 행동패턴 조건식 switch문 (공격 or 방어)
+        player.userHp -= resultMonster.monsterAttack;                      // 몬스터 행동 패턴 (공격 고정)
         print(
           "${resultMonster.monsterName}이(가) ${player.username}에게 ${resultMonster.monsterAttack -= player.userDefense}의 데미지를 입혔습니다.",
         );
-        if (resultMonster.monsterHp <= 0) {
+        if (resultMonster.monsterHp <= 0) {                                // 최상위 if, else if문 (몬스터 사망(몬스터 사망시 리스트에서 제거)/플레이어 사망)
           print("${resultMonster.monsterName}을 물리쳤습니다!");
           monsterList.remove(resultMonster);
-          if (monsterList.isNotEmpty) {
+          if (monsterList.isNotEmpty) {                                    // 하위 if, else if문문 (다음 몬스터 선택/모두 처치시 멘트 출력)
             print("다음 몬스터와 싸우시겠습니까? (y/n)");
             String? nextGameChoice = stdin.readLineSync();
-            if (nextGameChoice == "y") {
+            if (nextGameChoice == "y") {                                   // 최하위 if, else if문 (다음 게임 이동 Yes or No)
               print("다음 게임으로 이동합니다.");
-              break;
+              break;                                                       // 몬스터 처치시 내부 While문 탈출
             } else if (nextGameChoice == "n") {
               print("게임이 종료되었습니다.");
-              return;
+              return;                                                      // 게임 종료 (void mainbattle()탈출)
             }
-          }
-          else if (monsterList.isEmpty){
+          } else if (monsterList.isEmpty) {
             print("축하합니다!! 모든 몬스터를 물리쳤습니다.");
-          }
+          }                                                                // 리스트 내의 모든 몬스터 제거시 출력 멘트
         } else if (player.userHp <= 0) {
           print("패배하였습니다.");
           return;
-        }
+        }                                                                  // 플레이어 사망시 게임 종료
       }
     }
   }
 }
 
 void main() {
-  Game game = Game();
-  game.mainBattle();
+  Game game = Game();     // 게임 인스턴스 생성
+  game.mainBattle();      // 게임시작 
 }
